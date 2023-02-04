098fd6bb9f8ea9f209bbf7866df391527c769807
Naimhakimi <74091971+Naimhakimi@users.noreply.github.com>
2023-02-04 03:49:50 +0000

    py

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import streamlit as st
+from keras.models import load_model
+import numpy as np
 
 
 
+model = load_model('palm_oil_grader_100.h5')
 
-model = tf.keras.models.load_model('palm_oil_grader_100.h5')
+st.title("Grader")
 
-st.write("hi")
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
