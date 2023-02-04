82ac5f206059bfb42b2bb47e8adea4d58d93554f
Naimhakimi <74091971+Naimhakimi@users.noreply.github.com>
2023-02-04 03:42:29 +0000

    py

--- run.py
+++ run.py
@@ -1,17 +1,8 @@
-from flask import Flask, request, jsonify
+import streamlit as st
 import tensorflow as tf
 
-app = Flask(__name__)
+
 
 model = tf.keras.models.load_model('palm_oil_grader_100.h5')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    input_data = request.get_json()
-    image = input_data['image']
-    image = image.reshape(-1, 28, 28, 1)
-    prediction = model.predict(image).tolist()[0]
-    return jsonify({'prediction': prediction})
-
-if __name__ == '__main__':
-    app.run()
+st.write("hi")
